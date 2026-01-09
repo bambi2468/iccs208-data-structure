21b81ccbbb07a3479ac691d6ea3244e6b2dc46ed
pimnara <bamzazs2048@gmail.com>
2026-01-09 14:34:24 +0700

    Implement Diamond class with method to print diamond pattern

--- hw1/Diamond.java
+++ hw1/Diamond.java
@@ -1,9 +1,24 @@
-// package hw1;
+public class Diamond {
+    public static void printDiamond(int k) {
+        int totalChar = 2 * k + 1;
+        for (int i = 0; i < k; i++) {
+            int numSharp = k - i;
+            int numStar = totalChar - numSharp * 2;
+            String sharp = "#".repeat(numSharp);
+            String star = "*".repeat(numStar);
+            System.out.println(sharp + star + sharp);
+        }
+        for (int i = 0; i < k - 1; i++) {
+            int numSharp = 2 + i;
+            int numStar = totalChar - numSharp * 2;
+            String sharp = "#".repeat(numSharp);
+            String star = "*".repeat(numStar);
+            System.out.println(sharp + star + sharp);
+        }
+    }
 
-// public class Diamond {
-//     public static void printDiamond(int k){
-//         string star = "*";
+    public static void main(String[] args) {
+        printDiamond(3);
+    }
 
-//     }
-
-// }
+}
