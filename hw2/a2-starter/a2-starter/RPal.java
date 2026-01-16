a9db95954474afc0523006abafcd2ae6e252eda6
pimnara <bamzazs2048@gmail.com>
2026-01-16 12:26:10 +0700

    a2-starter

---
+++ hw2/a2-starter/a2-starter/RPal.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+public class RPal {
+    // store all the known answers, so we don't have to compute them again
+    private Map<Integer, List<List<Integer>>> storageAllRPals;
+
+
+    // a constructor: creates the above storage and puts in answers
+    // for our base cases of n = 0 and n = 1.
+    public RPal() {
+        storageAllRPals = new HashMap<>();
+        storageAllRPals.put(0, Arrays.asList(Arrays.asList()));
+        storageAllRPals.put(1, Arrays.asList(Arrays.asList(1)));
+    }
+
+		// Specification: return a list of integer lists of all recursively 
+		// palindromic sequences that sum to n.
+		//
+		// NOTE: When you implement this and want recursively-palindromic 
+		// sequences of k < n, you should not call yourself. Instead, you
+		// will call allRPals(k).  The difference is that computeAllRPals
+		// will compute the sequences every time whereas allRPals will *not* 
+		// compute the answer again unless it hasn't been computed before.
+    private List<List<Integer>> computeAllRPals(int n) {
+        // TODO: implement me
+
+        return null;
+    }
+
+    public List<List<Integer>> allRPals(int n) {
+        // Challenge: if you feel like learning a new trick, how would you
+        // rewrite the following using just storageAllRPals.computeIfAbsent(..)?
+
+        List<List<Integer>> answer = storageAllRPals.get(n);
+        if (answer == null) {
+            answer = computeAllRPals(n);
+            storageAllRPals.put(n, answer);
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        RPal rPal = new RPal();
+
+        System.out.println(rPal.allRPals(7));
+    }
+}
